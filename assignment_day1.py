7dfae9b5faa1ad055201442cca36a5f50a1836dc
neoabhic <41154744+neoabhic@users.noreply.github.com>
2021-08-17 10:17:50 +0530

    Assignment for day 1 submitted

---
+++ assignment_day1.py
@@ -0,0 +1,7 @@
+# Promt for the value of x and y
+x = int(input("Enter the first Number: "))
+y = int(input("Enter the second Number: 5")) 
+result = x**y
+# Printing the x to the power y
+print(f"{x} to the power {y} is {result}")
+
